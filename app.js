aea8c0903bd0e06fae5b6beefe1a421118e880ab
ajay297 <dangilajay9@gmail.com>
2021-04-21 11:07:27 +0530

    updated apikey

--- app.js
+++ app.js
@@ -23,25 +23,23 @@ io.on("connection", socket => {
 });
 
 const getApiAndEmit = async socket => {
-    setTimeout(async () => {
-        try {
-            const res = await axios.get(
-                "https://rest.coinapi.io/v1/exchangerate/ETH/USD?apikey=57E5962D-A44E-4890-99BB-9031885ACB40"
-            ); // Getting the data from DarkSky
-            const res1 = await axios.get(
-                "https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=57E5962D-A44E-4890-99BB-9031885ACB40"
-            );
-            console.log(res.data);
-            const data = { name: '', doge: res.data["rate"], btc: res1.data["rate"] };
-            // for (var i = 0; i < data.length; i++) {
-            //     rates.push({ name: "", value: data[i]["rate"] });
-            // }
-            console.log(data);
-            socket.emit("FromAPI", data); // Emitting a new message. It will be consumed by the client
-        } catch (error) {
-            console.error(`Error: ${error}`);
-        }
-    }, 1000)
+    try {
+        const res = await axios.get(
+            "https://rest.coinapi.io/v1/exchangerate/ETH/USD?apikey=3F96A0BF-0A87-4007-93A8-79EDEEA61657"
+        ); // Getting the data from DarkSky
+        const res1 = await axios.get(
+            "https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=3F96A0BF-0A87-4007-93A8-79EDEEA61657"
+        );
+        console.log(res.data);
+        const data = { name: '', doge: res.data["rate"], btc: res1.data["rate"] };
+        // for (var i = 0; i < data.length; i++) {
+        //     rates.push({ name: "", value: data[i]["rate"] });
+        // }
+        console.log(data);
+        socket.emit("FromAPI", data); // Emitting a new message. It will be consumed by the client
+    } catch (error) {
+        console.error(`Error: ${error}`);
+    }
 };
 
 server.listen(PORT, () => console.log(`Server running at port ${PORT}`));
